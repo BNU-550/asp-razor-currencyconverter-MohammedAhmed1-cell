4c2a96abcf0945f1063359a5781ca3bb4978e3e3
Mohammed Ahmed <71972232+MohammedAhmed1-cell@users.noreply.github.com>
2021-10-09 09:44:47 +0100

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Razor_Currency_Converter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        private string FullName { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Mohammed";
+        }
+
+        public void OnPost()
+        {
+            if(string.IsNullOrWhiteSpace(FullName))
+            {
+                ViewData["Message"] = "Name is missing!";
+                FullName = "Anonymous";
+            }
+            else
+            {
+                ViewData["Message"] = "Name is registered!";
+            }
+        }
+    }
+}
